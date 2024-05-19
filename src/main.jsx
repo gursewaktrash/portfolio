b32d51edbf6141cfe2111c9df05861cd91e0dfde
gursewaktrash <gursewaktrash@gmail.com>
2024-05-19 19:05:05 +0800

    Added Speed Insights
    Added Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import App from "./App.jsx";
 import Cursor from "./components/ui/Cursor.jsx";
 import "./index.css";
 
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Cursor />
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
