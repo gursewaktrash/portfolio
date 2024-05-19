dde4de5f0caa2f8742ca510b2fc3bdbff8e174be
gursewaktrash <gursewaktrash@gmail.com>
2024-05-19 19:09:01 +0800

    Remove Speed Insights
    Remove Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import Cursor from "./components/ui/Cursor.jsx";
 import "./index.css";
 
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Cursor />
     <App />
     <Analytics />
-    <SpeedInsights />
   </React.StrictMode>
 );
